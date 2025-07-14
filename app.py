85a21ff627c132959c1ca602ec64c3477c5c49b9
Josafat <josafat@example.com>
2025-07-14 12:19:55 -0600

    Subir2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
@@ -56,4 +57,5 @@ def index():
     return render_template("form.html", prediction=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
